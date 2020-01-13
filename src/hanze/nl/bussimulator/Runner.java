6b89504ac961e986400e7cb78b292ec5f966eeb0
Jeroen van der Laan <jeroen.jeroenvdlaan@gmail.com>
2020-01-13 14:26:08 +0100

    kleine runner refactor

--- src/hanze/nl/bussimulator/Runner.java
+++ src/hanze/nl/bussimulator/Runner.java
@@ -10,8 +10,9 @@ public class Runner {
 	private static ArrayList<Bus> actieveBussen = new ArrayList<Bus>();
 	private static int interval=1000;
 	
-	private static void addBus(int starttijd, Bus bus){
+	private static void addBusToBusStartAndSetID(int starttijd, Bus bus){
 		ArrayList<Bus> bussen = new ArrayList<Bus>();
+		// Als er al bussen op die starttijd beginnen, haal die op
 		if (busStart.containsKey(starttijd)) {
 			bussen = busStart.get(starttijd);
 		}
@@ -51,7 +52,7 @@ public class Runner {
 		}				
 	}
 	
-	public static int initBussen(){
+	public static void initBussen(){
 		Producer producer = new Producer();
 
 		Bus bus1=new Bus(Lijnen.LIJN1, Bedrijven.ARRIVA, 1, producer);
@@ -64,16 +65,16 @@ public class Runner {
 		Bus bus8=new Bus(Lijnen.LIJN1, Bedrijven.ARRIVA, 1, producer);
 		Bus bus9=new Bus(Lijnen.LIJN4, Bedrijven.ARRIVA, 1, producer);
 		Bus bus10=new Bus(Lijnen.LIJN5, Bedrijven.FLIXBUS, 1, producer);
-		addBus(3, bus1);
-		addBus(5, bus2);
-		addBus(4, bus3);
-		addBus(6, bus4);	
-		addBus(3, bus5);
-		addBus(5, bus6);
-		addBus(4, bus7); 
-		addBus(6, bus8);	
-		addBus(12, bus9); 
-		addBus(10, bus10);	
+		addBusToBusStartAndSetID(3, bus1);
+		addBusToBusStartAndSetID(5, bus2);
+		addBusToBusStartAndSetID(4, bus3);
+		addBusToBusStartAndSetID(6, bus4);
+		addBusToBusStartAndSetID(3, bus5);
+		addBusToBusStartAndSetID(5, bus6);
+		addBusToBusStartAndSetID(4, bus7);
+		addBusToBusStartAndSetID(6, bus8);
+		addBusToBusStartAndSetID(12, bus9);
+		addBusToBusStartAndSetID(10, bus10);
 		Bus bus11=new Bus(Lijnen.LIJN1, Bedrijven.ARRIVA, -1, producer);
 		Bus bus12=new Bus(Lijnen.LIJN2, Bedrijven.ARRIVA, -1, producer);
 		Bus bus13=new Bus(Lijnen.LIJN3, Bedrijven.ARRIVA, -1, producer);
@@ -84,23 +85,25 @@ public class Runner {
 		Bus bus18=new Bus(Lijnen.LIJN1, Bedrijven.ARRIVA, -1, producer);
 		Bus bus19=new Bus(Lijnen.LIJN4, Bedrijven.ARRIVA, -1, producer);
 		Bus bus20=new Bus(Lijnen.LIJN5, Bedrijven.FLIXBUS, -1, producer);
-		addBus(3, bus11);
-		addBus(5, bus12);
-		addBus(4, bus13);
-		addBus(6, bus14);	
-		addBus(3, bus15);
-		addBus(5, bus16);
-		addBus(4, bus17); 
-		addBus(6, bus18);	
-		addBus(12, bus19); 
-		addBus(10, bus20);	
-		return Collections.min(busStart.keySet());
+		addBusToBusStartAndSetID(3, bus11);
+		addBusToBusStartAndSetID(5, bus12);
+		addBusToBusStartAndSetID(4, bus13);
+		addBusToBusStartAndSetID(6, bus14);
+		addBusToBusStartAndSetID(3, bus15);
+		addBusToBusStartAndSetID(5, bus16);
+		addBusToBusStartAndSetID(4, bus17);
+		addBusToBusStartAndSetID(6, bus18);
+		addBusToBusStartAndSetID(12, bus19);
+		addBusToBusStartAndSetID(10, bus20);
 	}
+
+
 	
 	public static void main(String[] args) throws InterruptedException {
 		int tijd=0;
 		System.out.println("Init bussen");
-		int volgende = initBussen();
+		initBussen();
+		int volgende = Collections.min(busStart.keySet());
 		while ((volgende>=0) || !actieveBussen.isEmpty()) {
 			System.out.println("De tijd is:" + tijd);
 			volgende = (tijd==volgende) ? startBussen(tijd) : volgende;
